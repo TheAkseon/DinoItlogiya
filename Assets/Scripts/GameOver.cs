51cf42101d4e5b1c004467a4bf12b1483b5ba6ce
TheAkseon <81559799+TheAkseon@users.noreply.github.com>
2025-01-15 13:54:13 +0300

    добавил кнопку рестарта уровня

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private PlayerAnimations _playerAnimations;
+    [SerializeField] private Button _restartButton;
+
+    private void Start()
+    {
+        _restartButton.onClick.AddListener(SetRestartButton);
+        _restartButton.gameObject.SetActive(false);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -12,6 +21,13 @@ public class GameOver : MonoBehaviour
         {
             _playerAnimations.Die();
             Time.timeScale = 0f;
+            _restartButton.gameObject.SetActive(true);
         }
+    }
+
+    private void SetRestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
